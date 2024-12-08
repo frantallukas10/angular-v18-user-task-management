07a097845a11b0754e3a9c43dc23ec61b7c5e34f
Ing. Lukas Frantal <frantal.lukas10@gmail.com>
2024-12-08 21:16:39 +0100

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -9,9 +9,9 @@ export default {
   }
 ],
   assets: new Map([
-['index.csr.html', {size: 2535, hash: 'b9eeb247632f26c957f1c98f1465bcdf3cfad8e54b87b7f8e33ce04725018279', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
-['index.server.html', {size: 2743, hash: '2c77a078b6faca2971d8d425b86b3f96ba02e0eebd8caf76cf585c8e499ac626', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
-['t/index.html', {size: 8747, hash: 'd7a471fa218beec76b62bdf758b1ac5f998300107fcdb8f834e6dad17a9d565b', text: () => import('./assets-chunks/t_index_html.mjs').then(m => m.default)}], 
+['index.csr.html', {size: 2537, hash: '3054a977e3371e6bfbe84359080e898046cf8c19f7bde25a4c448937194da76d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
+['index.server.html', {size: 2745, hash: '99c5a85d113c2c1461c3b2515107531ee7fec8c3cb79757333855405afc03099', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
+['index.html', {size: 8749, hash: '081042330a2e82c4c5747d568a6d4726228d2c878a4e4c7c1708a2ba3df1df77', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)}], 
 ['styles-PH5QZW6A.css', {size: 186, hash: '08ViHY/xP6M', text: () => import('./assets-chunks/styles-PH5QZW6A_css.mjs').then(m => m.default)}]
 ]),
   locale: undefined,
--- server/assets-chunks/index_html.mjs
+++ server/assets-chunks/index_html.mjs
@@ -1,7 +1,7 @@
 export default `<!DOCTYPE html><html lang="en" data-beasties-container><head>
     <meta charset="utf-8">
     <title>EasyTrack</title>
-    <base href="angular-v18-user-task-management">
+    <base href="/angular-v18-user-task-management/">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
